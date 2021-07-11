bc05cd08e1de2897dbe72454b88e7349136aab25
Ferdinando de Melo <feoc10@gmail.com>
2021-07-11 19:23:57 -0300

    Aplicação Básica

---
+++ README.md
@@ -0,0 +1,3 @@
+# Diagnostico Diabetes App Android
+
+É um aplicativo para acessar o back-end do Diagnóstico Diabetes (https://github.com/feoc10/DiagnosticoDiabetes)
---
+++ app/src/main/java/com/feoc10/diagnosticodiabetesappandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.feoc10.diagnosticodiabetesappandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
